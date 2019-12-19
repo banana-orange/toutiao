127bf14baaea8ad1990adce47c08e5138568607d
futao <641627849@qq.com>
2019-12-19 21:02:12 +0800

    主页模块-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>主页</div>
+
+  <!-- 侧边栏 -->
+  <el-container>
+    <el-aside>我是侧边容器</el-aside>
+    <!-- 右侧大容器 -->
+    <el-container>
+      <!-- 顶部容器 -->
+      <el-header>
+        <el-header>我是顶部容器</el-header>
+      </el-header>
+
+      <el-main>我是主体容器</el-main>
+    </el-container>
+  </el-container>
+
 </template>
 
 <script>
