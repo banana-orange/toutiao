0eea6a1fbc5883bcf0ce14c819d00f2b7c136fdd
futao <641627849@qq.com>
2019-12-22 09:49:44 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import layoutAside from './home/layout-aside.vue'
 import layoutHeader from './home/layout-header.vue'
+import breadCrumb from './breadCrumb/bread-crumb.vue'
 
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
