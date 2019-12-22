7908b36acdc19eb210fa2cd6b76495a257685b07
futao <641627849@qq.com>
2019-12-22 10:32:09 +0800

    width

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>评论列表</div>
+  <el-card>
+      <bread-crumb slot="header">
+        <template slot="title">评论管理</template>
+      </bread-crumb>
+      <el-table>
+          <el-table-column label="标题" width='600'></el-table-column>
+          <el-table-column label="评论状态"></el-table-column>
+          <el-table-column label="总评论数"></el-table-column>
+          <el-table-column label="操作"></el-table-column>
+      </el-table>
+  </el-card>
 </template>
 
 <script>
