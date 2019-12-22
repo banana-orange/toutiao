7358c25fb2a3561ccd19ae69803a6a7f929f3b81
futao <641627849@qq.com>
2019-12-22 11:07:07 +0800

    评论数据加载到页面及状态过滤

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import VueRouter from 'vue-router'
 import login from '../views/login/index.vue'
 import home from '../views/home/index.vue'
 import home2 from '../views/home/home.vue'
-import comment from '../views/comment'
+// import comment from '../views/comment'
 
 Vue.use(VueRouter)
 
@@ -19,7 +19,7 @@ const routes = [
       },
       {
         path: '/home/comment',
-        component: comment
+        component: () => import('../views/comment')
       }
     ] }
   // {
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -3,10 +3,11 @@
       <bread-crumb slot="header">
         <template slot="title">评论管理</template>
       </bread-crumb>
-      <el-table>
-          <el-table-column label="标题" width='600'></el-table-column>
-          <el-table-column label="评论状态"></el-table-column>
-          <el-table-column label="总评论数"></el-table-column>
+      <el-table :data="list">
+          <el-table-column label="标题" width='600' prop='title'></el-table-column>
+          <el-table-column label="评论状态" :formatter='formatterBoolean' prop="comment_status"></el-table-column>
+          <el-table-column label="总评论数" prop='total_comment_count'></el-table-column>
+          <el-table-column label="粉丝评论数" prop='fans_comment_count'></el-table-column>
           <el-table-column label="操作"></el-table-column>
       </el-table>
   </el-card>
@@ -14,7 +15,30 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(res => {
+        console.log(res)
+        this.list = res.data.results
+      })
+    },
+    formatterBoolean (row, column, cellValue, index) {
+    //   console.log(row, column, cellValue, index)
+      console.log(cellValue)
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
+  }
 }
 </script>
 
