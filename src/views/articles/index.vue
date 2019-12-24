621be4dd12976a71bcdde69a4a1d8ebc6e5d4d66
futao <641627849@qq.com>
2019-12-24 22:47:38 +0800

    内容列表-页面结构-搜索筛选

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -6,7 +6,7 @@
 
     <el-form style="padding-left:50px">
       <el-form-item label="文章状态">
-        <el-radio-group v-model="searchForm.status">
+        <el-radio-group v-model="searchForm.status" @change="changeCondition">
           <el-radio :label="5">全部</el-radio>
           <el-radio :label="0">草稿</el-radio>
           <el-radio :label="1">待审核</el-radio>
@@ -18,12 +18,13 @@
         {{searchForm.dateRange}}
       </el-form-item>
       <el-form-item label="频道列表">
-        <el-select placeholder="请选择" v-model="searchForm.channel_id">
+        <el-select placeholder="请选择" v-model="searchForm.channel_id" @change="changeCondition">
           <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="时间选择">
         <el-date-picker
+        @change="changeCondition"
           v-model="searchForm.dateRange"
           value-format="yyyy-MM-dd"
           type="daterange"
@@ -100,6 +101,16 @@ export default {
     }
   },
   methods: {
+    changeCondition () {
+    //   alert(this.searchForm.status)
+      let params = {
+        status: this.searchForm.status === 5 ? null : this.searchForm.status,
+        channel_id: this.searchForm.channel_id,
+        begin_pubdate: this.searchForm.dateRange.length ? this.searchForm.dateRange[0] : null,
+        end_pubdate: this.searchForm.dateRange.length > 1 ? this.searchForm.dateRange[1] : null
+      }
+      this.getArticles(params)
+    },
     getChannels () {
       this.$axios({
         url: '/channels'
@@ -107,9 +118,10 @@ export default {
         this.channels = res.data.channels
       })
     },
-    getArticles () {
+    getArticles (params) {
       this.$axios({
-        url: '/articles'
+        url: '/articles',
+        params
       }).then(res => {
         console.log(res.data.results)
         this.list = res.data.results
