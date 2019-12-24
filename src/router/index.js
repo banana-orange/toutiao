28dd6142834d324d6441f173b92da63c5b974a7c
futao <641627849@qq.com>
2019-12-24 19:59:21 +0800

    内容列表-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,10 @@ const routes = [
       {
         path: 'material',
         component: () => import('../views/material')
+      },
+      {
+        path: 'articles',
+        component: () => import('../views/articles')
       }
     ] }
   // {
