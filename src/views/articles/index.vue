2d120a04e1ead433109d374c96dd727079c6d0d7
futao <641627849@qq.com>
2019-12-24 20:56:20 +0800

    内容列表-页面结构-搜索工具栏

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -1,17 +1,71 @@
 <template>
-<el-card>
+  <el-card>
     <bread-crumb slot="header">
-    <template slot="title">文章列表</template>
+      <template slot="title">文章列表</template>
     </bread-crumb>
-</el-card>
+
+    <el-form class="zong">
+      <el-form-item label="文章状态">
+        <el-radio-group v-model="searchForm.status">
+          <el-radio :label="5">全部</el-radio>
+          <el-radio :label="0">草稿</el-radio>
+          <el-radio :label="1">待审核</el-radio>
+          <el-radio :label="2">审核通过</el-radio>
+          <el-radio :label="3">审核失败</el-radio>
+        </el-radio-group>
+        {{searchForm.status }}
+        {{searchForm.channel_id}}
+        {{searchForm.dateRange}}
+      </el-form-item>
+      <el-form-item label="频道列表">
+        <el-select placeholder="请选择" v-model="searchForm.channel_id">
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="时间选择">
+        <el-date-picker
+          v-model="searchForm.dateRange"
+          value-format="yyyy-MM-dd"
+          type="daterange"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      searchForm: {
+        status: 5, // 文章状态
+        channel_id: null, // 频道
+        dateRange: [] // 时间
+      },
+      channels: []
+    }
+  },
+  methods: {
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(res => {
+        console.log(res)
+        this.channels = res.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
-</script>
+</script >
 
-<style>
+<style lang="less" scoped>
+    .zong{
+        padding-left: 50px;
+    }
 
 </style>
