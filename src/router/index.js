e305eae2dbb3564512b5a47a89a0debad1c8c97e
futao <641627849@qq.com>
2019-12-25 10:28:18 +0800

    发表文章-新建页面-挂载路由

--- src/router/index.js
+++ src/router/index.js
@@ -27,6 +27,10 @@ const routes = [
       {
         path: 'articles',
         component: () => import('../views/articles')
+      },
+      {
+        path: 'publish',
+        component: () => import('../views/publish')
       }
     ] }
   // {
