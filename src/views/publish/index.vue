56cde3656cb28d860bf7077fff02f9be2d23183b
futao <641627849@qq.com>
2019-12-25 10:56:36 +0800

    发表文章-页面结构-简单实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-      发布文章
-  </div>
+  <el-card>
+    <bread-crumb slot="header">
+      <el-input type="textarea" :rows="2" placeholder="请输入内容"></el-input>
+      <template slot="title">发布文章</template>
+    </bread-crumb>
+    <el-form label-width="100px" style="margin-left:50px">
+      <el-form-item label="标题：">
+        <!-- label-width="150px" -->
+        <el-input v-model="input" placeholder="文章名称" style="width:60%"></el-input>
+      </el-form-item>
+      <el-form-item label="内容：">
+        <el-input type="textarea" :rows="4" placeholder="请输入内容"></el-input>
+      </el-form-item>
+      <el-form-item label="封面：">
+            <el-radio-group v-model="radio">
+    <el-radio :label="3">单图</el-radio>
+    <el-radio :label="6">三图</el-radio>
+    <el-radio :label="9">无图</el-radio>
+    <el-radio :label="9">自动</el-radio>
+  </el-radio-group>
+      </el-form-item>
+      <el-form-item label="频道：">
+            <el-select v-model="value" placeholder="请选择">
+    <el-option
+      v-for="item in options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+    </el-option>
+  </el-select>
+
+      </el-form-item>
+      <el-row style="margin-left:95px">
+    <el-button type="primary">发布</el-button>
+      <el-button>存为草稿</el-button>
+      </el-row>
+    </el-form>
+  </el-card>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
-
 </style>
