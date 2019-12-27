88bfa6e42fa755bbe1d8b0cee11a5ca2c94815dd
futao <641627849@qq.com>
2019-12-27 22:34:53 +0800

    账户信息-用户数据保存

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -4,28 +4,28 @@
           <bread-crumb slot="header">
           <template slot="title">个人信息</template>
           </bread-crumb>
-          <el-form label-width="100px" style="margin-left:100px">
-              <el-form-item label="用户名：">
-                  <el-input style="width:40%" v-model="data.name"></el-input>
+          <el-form ref="myForm" label-width="100px" style="margin-left:100px" :model="formData" :rules="rules">
+              <el-form-item label="用户名：" prop="name">
+                  <el-input style="width:40%" v-model="formData.name"></el-input>
               </el-form-item>
               <el-form-item label="简介：">
-                  <el-input style="width:40%" v-model="data.intro"></el-input>
+                  <el-input style="width:40%" v-model="formData.intro"></el-input>
               </el-form-item>
-              <el-form-item label="邮箱：">
-                  <el-input style="width:40%" v-model="data.email"></el-input>
+              <el-form-item label="邮箱：" prop="email">
+                  <el-input style="width:40%" v-model="formData.email"></el-input>
               </el-form-item>
               <el-form-item label="手机号：">
-                  <el-input disabled style="width:40%"  v-model="data.mobile"></el-input>
+                  <el-input disabled style="width:40%"  v-model="formData.mobile"></el-input>
               </el-form-item>
               <el-form-item>
-                  <el-button type="primary">保存信息</el-button>
+                  <el-button @click="saveUserInfo" type="primary">保存信息</el-button>
               </el-form-item>
           </el-form>
 
           <el-upload action="" :show-file-list="false" class="shangchuan">
               <div>
                                 <p>更换头像</p>
-              <img :src="data.photo" alt="">
+              <img :src="formData.photo" alt="">
               </div>
           </el-upload>
       </el-card>
@@ -36,24 +36,47 @@
 export default {
   data () {
     return {
-      data: {
+      formData: {
         name: '',
         intro: '',
         photo: '',
         email: '',
         mobile: ''
+      },
+      rules: {
+        name: [{ required: true, message: '用户名不能为空' }, {
+          min: 1, max: 7, message: '用户名长度在1到7个字符'
+        }],
+        email: [{ required: true, message: '邮箱不能为空' }, {
+          pattern: /^([a-zA-Z]|[0-9])(\w|-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/,
+          message: '邮箱格式不正确'
+        }]
       }
     }
   },
   methods: {
+    saveUserInfo () {
+      this.$refs.myForm.validate().then(() => {
+        this.$axios({
+          url: '/user/profile',
+          method: 'patch',
+          data: this.formData
+        }).then(res => {
+          this.$message({
+            type: 'success',
+            message: '保存用户信息成功'
+          })
+        })
+      })
+    },
     getUserInfo () {
       this.$axios({
         url: '/user/profile'
       }).then(res => {
-        console.log(res)
-        this.data = res.data
+        this.formData = res.data
       })
     }
+
   },
   created () {
     this.getUserInfo()
